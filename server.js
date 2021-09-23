8d5d803a85284ef91b5d4ec08c6aa54dc1256c41
Aravind <aravindpc7559@gmail.com>
2021-09-23 22:26:42 +0530

    fifth video part5 today created cart items session etc....

--- server.js
+++ server.js
@@ -1,34 +1,61 @@
+require("dotenv").config()
 const  express = require("express");
 const ejs = require("ejs");
 const expressLayout = require("express-ejs-layouts");
 const path = require("path")
 const app = express();
 const PORT = process.env.PORT || 3300;
+const mongoose = require("mongoose")
+const session = require("express-session")
+const flash  = require("express-flash")
+var MongoDbStore = require('connect-mongodb-session')(session);
+
+
+// Database connection
+mongoose.connect(process.env.MONGO_CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true,  }).then(()=>{
+console.log("database connected")
+}).catch((err)=>{
+    console.log("'error")
+})
+
+// session store..
+let  mongoStore = new MongoDbStore({
+    uri:'mongodb://localhost/pizza',
+    // mongooseConnection: sessionDb,
+    collection: 'sessions',
+})
+
+// sessions config....
+
+app.use(session({
+    secret: process.env.COOKIE_SECRET,
+    resave: false,
+    store:mongoStore,
+    saveUninitialized: false,
+    cookie: { maxAge: 1000 * 60 * 60 * 24 } // 24 hour
+}))
+
+app.use(flash())
 
 //assets
 app.use(express.static('public'))
+app.use(express.json())
 
+
+//global middleware..
+app.use((req,res,next)=>{
+res.locals.session = req.session
+next()
+})
 //set template engine
 app.use(expressLayout)
 app.set('views',path.join(__dirname,'/resources/views'))
 app.set('view engine','ejs')
 
 
-app.get('/',(req,res)=>{
-    res.render('home')
-})
+require('./routes/web')(app)
 
-app.get('/cart',(req,res)=>{
-    res.render('customers/cart')
-})
 
-app.get('/login',(req,res)=>{
-    res.render('auth/login')
-})
-
-app.get('/register',(req,res)=>{
-    res.render('auth/register')
-})
 
 
 app.listen(PORT,()=>console.log(`server running on ${PORT}`))
